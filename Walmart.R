548752f16a0f67691142fa525ce4b429f47c17ab
Yaseen Lotfi <yalotfi@outlook.com>
2016-09-19 12:13:39 -0400

    Update Walmart.R

--- Walmart.R
+++ Walmart.R
@@ -1,35 +1,12 @@
-
-rm(list = ls())
-
-##############
-## Packages ##
-##############
-## Basic Packages
-library(XML)
-library(ggplot2)
-library(ggmap)
-library(dplyr)
-library(animation)
-library(spatstat)
-
-## GIS Packages
-library(raster)
-library(rgdal)
-library(rgeos)
-library(ggmap)
-library(sp)
-library(maptools)
-
+source("sourcePackages.R")
 
 ########################
 ## Parse Walmart Data ##
 ########################
 ## Bring Data and Make Helper
-wmData <- readLines("walmarts.xml") # view raw data to see variables
-strsplits <- function(x, splits, ...) # Rewrite strsplit() function
-{
-  for (split in splits)
-  {
+wmData <- readLines("Data/walmarts.xml") # view raw data to see variables
+strsplits <- function(x, splits, ...) {   # Rewrite strsplit() function
+  for (split in splits) {
     x <- unlist(strsplit(x, split, ...))
   }
   return(x[!x == ""]) # Remove empty values
@@ -60,11 +37,23 @@ wmData <- cbind.data.frame(name, year, lat, long)
 wmData$id <- 1:nrow(wmData)
 rm(name, year, lat, long)
 
+## Bring in US Spatial Data to df
+states <- spTransform(readOGR(dsn = "USAshp", layer = "states", stringsAsFactors = FALSE, verbose = FALSE), CRS("+proj=longlat +datum=WGS84"))
+statesdf <- states@data # Bring slotted data into dataframe
+statesdf <- statesdf[order(statesdf$DRAWSEQ),] # order ID, low to high
+statesdf <- filter(statesdf, STATE_ABBR != "HI" & STATE_ABBR != "AK")  # Remove states out of bounds
+statesdf$DRAWSEQ <- as.character(statesdf$DRAWSEQ) # join columns must have same variable type, convert to string
+statesFortified <- fortify(states, region = "DRAWSEQ")
+statesFortified <- statesFortified[order(as.numeric(statesFortified$id)),]
+USAmain <- inner_join(statesdf, statesFortified, by = c("DRAWSEQ" = "id"))
+rm(states, statesdf, statesFortified)
 
-#####################
-## Data Management ##
-#####################
-## Explore a little
+
+
+
+######################
+## Explore the Data ##
+######################
 Growth <- wmData$year
 time <- max(Growth) - min(Growth) # 44 years
 pdf("Output/Walmart_Histogram.pdf")
@@ -151,86 +140,23 @@ dev.off()
 rm(wm1970, wm1980, wm1990, wm2000, wm2010) # remove subsets
 rm(map1970, map1980, map1990, map2000, map2010) # remove maps
 
-
-################
-## Nicer Maps ##
-################
-## Help with nicer map
-alpha_range = c(0.14, 0.75)
-size_range = c(0.134, 0.173)
-
-## Create a title with subtitle in ggplot/ggmap
-ggtitle_subtitle = function(title, subtitle = "") {
-  
-  ggtitle(bquote(atop(bold(.(title)), atop(.(subtitle)))))
-
-  }
-
-## Black and White Theme Map
-fontCheck <- names(pdfFonts())
-
-BWtheme = function(base_size = 12) {
-  
-  theme_bw(base_size) +
-    theme(text = element_text(color = "#ffffff"),
-          rect = element_rect(fill = "#000000", color = "#000000"),
-          plot.background = element_rect(fill = "#000000", color = "#000000"),
-          panel.background = element_rect(fill = "#000000", color = "#000000"),
-          plot.title = element_text(),
-          panel.grid = element_blank(),
-          panel.border = element_blank(),
-          axis.text = element_blank(),
-          axis.title = element_blank(),
-          axis.ticks = element_blank())
-  
-  }
-
-
-# family = "Sans",
-
-## Bring in US Spatial Data to df
-states <- spTransform(readOGR(dsn = "USAshp", layer = "states", stringsAsFactors = FALSE, verbose = FALSE), CRS("+proj=longlat +datum=WGS84"))
-statesdf <- states@data # Bring slotted data into dataframe
-statesdf <- statesdf[order(statesdf$DRAWSEQ),] # order ID, low to high
-statesdf <- filter(statesdf, STATE_ABBR != "HI" & STATE_ABBR != "AK")  # Remove states out of bounds
-statesdf$DRAWSEQ <- as.character(statesdf$DRAWSEQ) # join columns must have same variable type, convert to string
-statesFortified <- fortify(states, region = "DRAWSEQ")
-statesFortified <- statesFortified[order(as.numeric(statesFortified$id)),]
-USAmain <- inner_join(statesdf, statesFortified, by = c("DRAWSEQ" = "id"))
-rm(states, statesdf, statesFortified)
-
-## Build Map
-US <- ggplot()
-US <- US + geom_polygon(data = USAmain, aes(x = long, y = lat, group = group), fill = "#080808", color = "#080808")
-US <- US + geom_point(data = wmData, aes(x = long, y = lat, alpha = id, size = id), color = "dodgerblue")
-US <- US + scale_alpha_continuous(range  = alpha_range, trans = "log", limits = range(wmData$id))
-US <- US + scale_size_continuous(range  = size_range, trans = "log", limits = range(wmData$id))
-US <- US + scale_color_manual(values = c("yellow", "green", "gray", "red", "blue"))
-US <- US + coord_map(xlim = range(USAmain$long), ylim = range(USAmain$lat))
-US <- US + ggtitle_subtitle("3,000 Walmart Locations", "Built Between 1962 and 2006")  # Problem with fonts
-US <- US + BWtheme(base_size = 24)
-US <- US + theme(legend.position = "none")
-ggsave("Output/WalMartUSA.pdf", US)
-ggsave("Output/WalMartUSA.png", US)
-
-
 ## remove objs
-rm(bbox, fontCheck, waterMap, alpha_range, size_range)
+rm(bbox, waterMap)
 
 #####################
 ## Spatial Density ##
 #####################
 ## Create Point Pattern Window
-states <- spTransform(readOGR(dsn = "USAshp", layer = "states", stringsAsFactors = FALSE, verbose = FALSE), CRS("+proj=longlat +datum=WGS84"))
-states@data <- filter(states@data, STATE_ABBR != "HI" & STATE_ABBR != "AK")
-win <- as(states,"owin")
-
-## Create Point Pattern Class Objs
-pppWM <- ppp(x = wmData$long, y = wmData$lat, window = win, marks = wmData$year)
-plot(density(pppWM))
-
-## remove objs
-rm(states, win, pppWM)
+# states <- spTransform(readOGR(dsn = "USAshp", layer = "states", stringsAsFactors = FALSE, verbose = FALSE), CRS("+proj=longlat +datum=WGS84"))
+# states@data <- filter(states@data, STATE_ABBR != "HI" & STATE_ABBR != "AK")
+# win <- as(states,"owin")
+# 
+# ## Create Point Pattern Class Objs
+# pppWM <- ppp(x = wmData$long, y = wmData$lat, window = win, marks = wmData$year)
+# plot(density(pppWM))
+# 
+# ## remove objs
+# rm(states, win, pppWM)
 
 
 
