3b004abac5102e6dba5fc90c78df79c57f5058ad
Yaseen Lotfi <yalotfi@outlook.com>
2016-09-19 12:07:12 -0400

    Create sourcePackages.R
    Source file for packages - organization

---
+++ sourcePackages.R
@@ -0,0 +1,21 @@
+##############
+## Packages ##
+##############
+# install.packages("", dependencies = T)  # Install packages as needed
+
+## Basic Packages
+library(ggplot2)
+library(ggmap)
+library(dplyr)
+
+## GIS Packages
+library(raster)
+library(rgdal)
+library(rgeos)
+library(sp)
+library(maptools)
+
+## For Walmart
+library(XML)       # Parse XML
+library(animation) # Animate plots, kind of clunky
+library(spatstat)  # Perform spatial statistics
